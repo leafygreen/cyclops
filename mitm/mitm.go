fe806957cbc083bfc2208ad794605d9ee8a9f682
Stephen Lee <slee@heymrtough.com>
2015-12-17 11:42:57 -0500

    Serve static files and collapse all endpoints into same HTTP instance

--- mitm/mitm.go
+++ mitm/mitm.go
@@ -6,71 +6,88 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-    "io"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strconv"
 	"strings"
 )
 
 type Listeners struct {
-    connections []*websocket.Conn
-    message chan interface{}
+	// FIXME this sucks and should be replaced with something the prunes dead/closed connections
+	connections []*websocket.Conn
+	message     chan interface{}
 }
 
 func (l *Listeners) Broadcast() {
-    for {
-        jsonData := <- l.message
+	for {
+		jsonData := <-l.message
 
-        if jsonData == nil {
-            continue
-        }
+		if jsonData == nil {
+			continue
+		}
 
-        buf, err := json.Marshal(jsonData)
+		buf, err := json.Marshal(jsonData)
 
-        if err != nil {
-            fmt.Println(err.Error())
-            continue
-        }
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
 
-        for _, ws := range l.connections {
-            if ws != nil {
-                _, err2 := ws.Write(buf)
+		for _, ws := range l.connections {
+			if ws != nil {
+				_, err2 := ws.Write(buf)
 
-                if err2 != nil {
-                    fmt.Println(err2.Error())
-                }
-            }
-        }
-    }
+				if err2 != nil {
+					fmt.Println(err2.Error())
+				}
+			}
+		}
+	}
 }
 
 func (l *Listeners) Add(ws *websocket.Conn) {
-    l.connections = append(l.connections, ws)
-    io.Copy(ws, ws)
+	l.connections = append(l.connections, ws)
+	io.Copy(ws, ws)
+}
+
+type SelectiveProxy struct {
+	prefixes       map[string]http.Handler
+	defaultHandler http.Handler
+}
+
+func (s SelectiveProxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	requestUrlStr := req.URL.String()
+
+	for prefix, handler := range s.prefixes {
+		// FIXME this is crappy and should be replaced with a trie or something just better
+		if strings.Index(requestUrlStr, prefix) == 0 {
+			// call the first handler whose prefix matches the starf of the requestUrlStr
+			handler.ServeHTTP(w, req)
+			return
+		}
+	}
+
+	s.defaultHandler.ServeHTTP(w, req)
 }
 
 func main() {
 	urlStr := flag.String("url", "http://localhost:8080", "MMS Backend URL")
+	proxyPort := flag.Int("port", 9090, "Reverse Proxy Port")
+	staticPath := flag.String("staticPath", "../webapp/dist", "Cyclops Frontend")
 	flag.Parse()
 
 	mmsUrl, err := url.Parse(*urlStr)
 
 	if err != nil {
-        panic("Could not parse url: " + err.Error())
-    }
+		panic("Could not parse url: " + err.Error())
+	}
 
-    message := make(chan interface{})
-    listeners := Listeners{connections: make([]*websocket.Conn, 0), message: message}
-
-    go listeners.Broadcast()
-
-    go func() {
-        http.Handle("/", websocket.Handler(listeners.Add))
-        http.ListenAndServe(":12345", nil)
-    }()
-
+	message := make(chan interface{})
+	listeners := Listeners{connections: make([]*websocket.Conn, 0), message: message}
+	go listeners.Broadcast()
 	proxy := httputil.NewSingleHostReverseProxy(mmsUrl)
 
 	// override director to intercept request body
@@ -84,26 +101,25 @@ func main() {
 			err := json.Unmarshal(buf, &jsonData)
 
 			if err == nil {
-                requestUrlStr := request.URL.String()
-                queryParams, _ := url.ParseQuery(request.URL.RawQuery)
+				requestUrlStr := request.URL.String()
+				queryParams, _ := url.ParseQuery(request.URL.RawQuery)
 
-                // copy jsonData to websocket listeners
-                wrapper := make(map[string]interface{})
-                wrapper["remoteAddr"] = request.RemoteAddr
-                wrapper["ah"] = queryParams.Get("ah")
-                wrapper["type"] = nil
+				// copy jsonData to websocket listeners
+				wrapper := make(map[string]interface{})
+				wrapper["remoteAddr"] = request.RemoteAddr
+				wrapper["ah"] = queryParams.Get("ah")
+				wrapper["type"] = nil
 
-                if strings.Index(requestUrlStr, "/agents/api/automation/metrics") == 0 {
-                    wrapper["type"] = "metrics"
-                } else if strings.Index(requestUrlStr, "/agents/api/automation/status") == 0 {
-                    wrapper["type"] = "status"
-                } else if strings.Index(requestUrlStr, "/agents/api/automation/log") == 0 {
-                    wrapper["type"] = "log"
-                }
+				if strings.Index(requestUrlStr, "/agents/api/automation/metrics") == 0 {
+					wrapper["type"] = "metrics"
+				} else if strings.Index(requestUrlStr, "/agents/api/automation/status") == 0 {
+					wrapper["type"] = "status"
+				} else if strings.Index(requestUrlStr, "/agents/api/automation/log") == 0 {
+					wrapper["type"] = "log"
+				}
 
-                wrapper["content"] = jsonData
-                message <- wrapper
-				//fmt.Println(jsonData)
+				wrapper["content"] = jsonData
+				message <- wrapper
 			}
 
 			request.Body = ioutil.NopCloser(bytes.NewReader(buf))
@@ -112,5 +128,9 @@ func main() {
 		oldDirector(request)
 	}
 
-	http.ListenAndServe(":9090", proxy)
+	prefixes := make(map[string]http.Handler)
+	prefixes["/ws"] = websocket.Handler(listeners.Add)
+	prefixes["/agents/api/automation/"] = proxy
+	selectiveProxy := SelectiveProxy{prefixes: prefixes, defaultHandler: http.FileServer(http.Dir(*staticPath))}
+	http.ListenAndServe(":"+strconv.Itoa(*proxyPort), selectiveProxy)
 }
